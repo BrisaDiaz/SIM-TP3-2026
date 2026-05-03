a366370b66f4a96a9201cf81ecfd320443987e7f
Brisa Abigaíl Díaz <brisaabigaildiaz2000@gmail.com>
2026-05-02 21:15:32 -0300

    service v1

--- src/FormularioDatosModificables.jsx
+++ src/FormularioDatosModificables.jsx
@@ -1,116 +1,296 @@
-import { useEffect, useState } from "react"
-import {useForm} from "react-hook-form"
-import {useNavigate, useParams, useLocation} from "react-router-dom"
+//import { useEffect, useState } from "react"
+import { useForm } from 'react-hook-form';
+//import {useNavigate, useParams, useLocation} from "react-router-dom"
 
+const FormularioDatosModificables = ({ onSubmitData }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+    getValues,
+  } = useForm({
+    defaultValues: {
+      filasSimular: 100000,
+      filasMostrar: 200,
+      filaDesde: 1,
+      probCercano: 35,
+      probIntermedio: 40,
+      probLejano: 25,
+      aCercano: 1,
+      bCercano: 2,
+      aIntermedio: 2,
+      bIntermedio: 3,
+      aLejano: 3,
+      bLejano: 5,
+      aRecorrido: 30,
+      bRecorrido: 45,
+      probValidacion: 45,
+      mediaValidacion: 60,
+      desvValidacion: 20,
+      probBloqueo: 40,
+      porcAumentoDemora: 80,
+      probParadaExtra: 24,
+      mediaParadaExtra: 80,
+    },
+  });
+  const onSubmit = async (data) => {
+    try {
+      onSubmitData(data);
+    } catch (error) {
+      console.error(
+        'Error al cargar los párametros:',
+        error.response?.data || error.message,
+      );
+      alert(error.response?.data?.error || 'Error al guardar función');
+    }
+  };
 
-const FormularioDatosModificables = () =>{
-    const [cines, setCines] = useState([]);
-    const {id} = useParams();
-    const navigate = useNavigate();
-    const {register, handleSubmit, reset, formState:{errors}, getValues} = useForm();
-    const location = useLocation();
-    const modo = location.state?.modo
-    const [originalData, setOriginalData] = useState(null);
-
-    return (
-        <main className="container mt-5">
-      <h3 className="mb-4">: TP 3 - Parqueando</h3>
-      <form onSubmit={handleSubmit(onSubmit)} className="row g-3">
-        <div className="col-md-6">
-          <label>Pelicula:</label>
-          <input {...register("pelicula", { required: true })} className="form-control" />
-          {errors.pelicula && <span className="text-danger">Campo obligatorio</span>}
-        </div>
-       <div className="col-md-6">
-         <label>Fecha desde:</label>
-        <input
-                {...register("fechaDesde", {
-                required: true,
-                validate: (value) => {
-                    const dia = new Date(value).getDay();
-                    return dia === 3 || "La fecha debe ser un jueves"; }
-            })}
-            className="form-control"
-            type="date"
-            />
-        {errors.fechaDesde && (
-            <span className="text-danger">{errors.fechaDesde.message || "Campo obligatorio"}</span>
-                    )}
-            </div>
-        <div className="col-md-6">
-  <label>Fecha hasta:</label>
-  <input
-    {...register("fechaHasta", {
-      required: "Campo obligatorio",
-      validate: {
-        noPasada: (value) => {
-          const hoy = new Date();
-          const fecha = new Date(value);
-          return fecha >= hoy || "La fecha no puede ser anterior a hoy";
-        },
-        diferencia6Dias: (value) => {
-          const fechaDesde = new Date(getValues("fechaDesde"));
-          const fechaHasta = new Date(value);
-          const diff = Math.abs(fechaHasta - fechaDesde);
-          const dias = diff / (1000 * 60 * 60 * 24);
-          return dias === 6 || "La diferencia con fecha desde debe ser de 6 días";
-        }
-      }
-        })}
-        className="form-control"
-        type="date"
-        />
-        {errors.fechaHasta && (
-            <span className="text-danger">{errors.fechaHasta.message}</span>
-            )}
+  return (
+    <main className='container mt-5'>
+      <h3 className='mb-4'>TP Simulación Parqueando</h3>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='row g-3'
+      >
+        <div className='col-md-6'>
+          <label>Cantidad de filas a simular:</label>
+          <input
+            {...register('filasSimular', { required: true })}
+            className='form-control'
+          />
+          {errors.filasSimular && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
         </div>
 
-        <div className="col-md-6">
-          <label>Horario:</label>
-          <input {...register("horario", { required: true })} className="form-control" />
-          {errors.horario && <span className="text-danger">Campo obligatorio</span>}
+        <div className='col-md-6'>
+          <label>Cantidad de filas a mostrar:</label>
+          <input
+            {...register('filasMostrar', { required: true })}
+            className='form-control'
+          />
+          {errors.filasMostrar && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
         </div>
-        <div className="col-md-6">
-          <label>Sala:</label>
-          <input {...register("sala", { required: true })} className="form-control" />
-          {errors.sala && <span className="text-danger">Campo obligatorio</span>}
+
+        <div className='col-md-6'>
+          <label>Mostrar desde la fila :</label>
+          <input
+            {...register('filaDesde', { required: true })}
+            className='form-control'
+          />
+          {errors.filaDesde && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
         </div>
-        <div className="col-md-6">
-          <label>Cine:</label>
-          <select {...register("idCine", { required: true })} className="form-select">
-            <option value="">Seleccione</option>
-            {cines.map((cine) => (
-              <option key={cine.id} value={cine.id}>{cine.nombre}</option>
-            ))}
-          </select>
-          {errors.idCine && <span className="text-danger">Campo obligatorio</span>}
+
+        <h3 className='mb-4 mt-4'>Sector Cercano </h3>
+        <div className='col-md-6'>
+          <label>Porcentaje:</label>
+          <input
+            {...register('probCercano', { required: true })}
+            className='form-control'
+          />
+          {errors.probCercano && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
         </div>
-        <div className="col-md-6">
-  <label>URL de la carátula:</label>
-  <input
-    {...register("caratula", {
-      validate: value => {
-        if (!value) return true; // Si está vacío, pasa la validación porque no es obligatorio
-        try {
-          new URL(value); // Intenta crear un objeto URL para validar
-          return true;
-        } catch {
-          return "Debe ser una URL válida";
-        }
-      }
-    })}
-    className="form-control"
-    type="url"
-    placeholder="https://ejemplo.com"
-  />
-  {errors.caratula && <span className="text-danger">{errors.caratula.message}</span>}
-</div>
-        <div className="col-12">
-          <button type="submit" className="btn btn-success">Guardar</button>
+        <div className='col-md-6'>
+          <label>A:</label>
+          <input
+            {...register('aCercano', { required: true })}
+            className='form-control'
+          />
+          {errors.aCercano && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+        <div className='col-md-6'>
+          <label>B:</label>
+          <input
+            {...register('bCercano', { required: true })}
+            className='form-control'
+          />
+          {errors.bCercano && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+
+        <h3 className='mb-4 mt-4'>Sector Intermedio </h3>
+        <div className='col-md-6'>
+          <label>Porcentaje:</label>
+          <input
+            {...register('probIntermedio', { required: true })}
+            className='form-control'
+          />
+          {errors.probIntermedio && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+        <div className='col-md-6'>
+          <label>A:</label>
+          <input
+            {...register('aIntermedio', { required: true })}
+            className='form-control'
+          />
+          {errors.aIntermedio && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+        <div className='col-md-6'>
+          <label>B:</label>
+          <input
+            {...register('bIntermedio', { required: true })}
+            className='form-control'
+          />
+          {errors.bIntermedio && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+
+        <h3 className='mb-4 mt-4'>Sector Lejano </h3>
+        <div className='col-md-6'>
+          <label>Porcentaje:</label>
+          <input
+            {...register('probLejano', { required: true })}
+            className='form-control'
+          />
+          {errors.probLejano && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+        <div className='col-md-6'>
+          <label>A:</label>
+          <input
+            {...register('aLejano', { required: true })}
+            className='form-control'
+          />
+          {errors.aLejano && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+        <div className='col-md-6'>
+          <label>B:</label>
+          <input
+            {...register('bLejano', { required: true })}
+            className='form-control'
+          />
+          {errors.bLejano && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+
+        <h3 className='mb-4 mt-4'>Párametros para el recorrido </h3>
+        <div className='col-md-6'>
+          <label>A:</label>
+          <input
+            {...register('aRecorrido', { required: true })}
+            className='form-control'
+          />
+          {errors.aRecorrido && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+        <div className='col-md-6'>
+          <label>B:</label>
+          <input
+            {...register('bRecorrido', { required: true })}
+            className='form-control'
+          />
+          {errors.bRecorrido && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+
+        <h3 className='mb-4 mt-4'>Parada de validación </h3>
+        <div className='col-md-6'>
+          <label>Porcentaje de que frente:</label>
+          <input
+            {...register('probValidacion', { required: true })}
+            className='form-control'
+          />
+          {errors.probValidacion && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+        <div className='col-md-6'>
+          <label>Media:</label>
+          <input
+            {...register('mediaValidacion', { required: true })}
+            className='form-control'
+          />
+          {errors.mediaValidacion && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+        <div className='col-md-6'>
+          <label>Desviación Estándar:</label>
+          <input
+            {...register('desvValidacion', { required: true })}
+            className='form-control'
+          />
+          {errors.desvValidacion && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+
+        <h3 className='mb-4 mt-4'>Bloqueo de Ruta </h3>
+        <div className='col-md-6'>
+          <label>Porcentaje de que haya bloqueo:</label>
+          <input
+            {...register('probBloqueo', { required: true })}
+            className='form-control'
+          />
+          {errors.probBloqueo && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+        <div className='col-md-6'>
+          <label>Porcentaje de aumento de demora:</label>
+          <input
+            {...register('porcAumentoDemora', { required: true })}
+            className='form-control'
+          />
+          {errors.porcAumentoDemora && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+
+        <h3 className='mb-4 mt-4'>Parada extra </h3>
+        <div className='col-md-6'>
+          <label>Porcentaje de que haya una parada extra:</label>
+          <input
+            {...register('probParadaExtra', { required: true })}
+            className='form-control'
+          />
+          {errors.probParadaExtra && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+        <div className='col-md-6'>
+          <label>Media:</label>
+          <input
+            {...register('mediaParadaExtra', { required: true })}
+            className='form-control'
+          />
+          {errors.mediaParadaExtra && (
+            <span className='text-danger'>Campo obligatorio</span>
+          )}
+        </div>
+
+        <div className='col-12'>
+          <button
+            type='submit'
+            className='btn btn-success'
+          >
+            Ejecutar
+          </button>
         </div>
       </form>
     </main>
-    )
-}
+  );
+};
 
-export default FormularioFunciones
+export default FormularioDatosModificables;
---
+++ src/servicio/inidex.js
@@ -0,0 +1,311 @@
+import {
+  GeneradorExponencial,
+  GeneradorNormal,
+  GeneradorUniforme,
+  generarRND,
+} from '../generadores';
+
+
+const MOCK_DATOS = {
+    "filasSimular": 100000,
+    "filasMostrar": 200,
+    "filaDesde": 0,
+    "probCercano": 35,
+    "probIntermedio": 40,
+    "probLejano": 25,
+    "aCercano": 1,
+    "bCercano": 2,
+    "aIntermedio": 2,
+    "bIntermedio": 3,
+    "aLejano": 3,
+    "bLejano": 5,
+    "aRecorrido": 30,
+    "bRecorrido": 45,
+    "probValidacion": 45,
+    "mediaValidacion": 60,
+    "desvValidacion": 20,
+    "probBloqueo": 40,
+    "porcAumentoDemora": 80,
+    "probParadaExtra": 24,
+    "mediaParadaExtra": 80
+}
+
+export function ServicioSimulacion(datos) {
+  //Generadores
+    const generadorUniformeCuadrasSectorCercano = new GeneradorUniforme(datos.aCercano, datos.bCercano);
+    const generadorUniformeCuadrasSectorIntermedio = new GeneradorUniforme(datos.aIntermedio, datos.bIntermedio);
+    const generadorUniformeCuadrasSectorLejano = new GeneradorUniforme(datos.aLejano, datos.bLejano);
+    const generadorUniformeRecorrido = new GeneradorUniforme(datos.aRecorrido, datos.bRecorrido);
+    const generadorNormalValidacion = new GeneradorNormal(datos.mediaValidacion, datos.desvValidacion);
+    const generadorExponencialParadaExtra = new GeneradorExponencial(1 / datos.mediaParadaExtra);
+    
+    const generadores = {
+        generadorUniformeCuadrasSectorCercano,
+        generadorUniformeCuadrasSectorIntermedio,
+        generadorUniformeCuadrasSectorLejano,
+        generadorUniformeRecorrido,
+        generadorNormalValidacion,
+        generadorExponencialParadaExtra,
+    };
+    //Simulación
+    const vectorEstado = [];
+
+    let filaAnterior = [];
+    let filaActual = [];
+
+    for (let i = 0; i < datos.filasSimular; i++) {
+
+        filaActual = procesarFila(filaAnterior, datos, generadores); // procesar fila actual a partir de fila anterior
+        
+        
+        if (i + 1 >= datos.filaDesde + 1  && vectorEstado.length < datos.filasMostrar) { 
+            // guardar fila en vectorEstado
+            vectorEstado.push(filaActual);
+        }
+        filaAnterior = [...filaActual]; // actualizar fila anterior
+    }
+
+
+    const reporte = generarReporte(filaAnterior);
+
+    return {
+        vectorEstado,
+        reporte,
+        ultimaFila: filaAnterior,
+    }
+    
+}
+
+function determinarSectorDestino(rnd, datos) {
+    const rangoCercano = [0, datos.probCercano / 100 - 0.01];
+    const rangoIntermedio = [datos.probCercano / 100, (datos.probCercano + datos.probIntermedio) / 100 - 0.01];
+    
+   if (rnd >= rangoCercano[0] && rnd <= rangoCercano[1]) {
+        return 'Cercano';
+   } else if (rnd >= rangoIntermedio[0] && rnd <= rangoIntermedio[1]) {
+        return 'Intermedio';
+    }
+    return 'Lejano';
+}
+
+function determinarCuadrasAlDestino(sector, rnd, generadores) {
+    switch (sector) {
+        case 'Cercano':
+            return generadores.generadorUniformeCuadrasSectorCercano.generar(rnd)[0];
+        case 'Intermedio':
+            return generadores.generadorUniformeCuadrasSectorIntermedio.generar(rnd)[0];
+        case 'Lejano':
+            return generadores.generadorUniformeCuadrasSectorLejano.generar(rnd)[0];
+        default:
+            throw new Error('Sector desconocido');
+    }
+
+}
+
+function derminarParadaValidacion(rnd, datos) {
+    const rangoValidacion = [0, datos.probValidacion / 100 - 0.01];
+    return rnd >= rangoValidacion[0] && rnd <= rangoValidacion[1];
+}
+
+function determinarBloqueoRuta(rnd, datos) {
+    const rangoBloqueo = [0, datos.probBloqueo / 100 - 0.01];
+    return rnd >= rangoBloqueo[0] && rnd <= rangoBloqueo[1];
+}
+
+function determinarParadaExtra(rnd, datos) {
+    const rangoParadaExtra = [0, datos.probParadaExtra / 100 - 0.01];
+    return rnd >= rangoParadaExtra[0] && rnd <= rangoParadaExtra[1];
+}
+
+function determinarDemoraValidacion(rnd1, rnd2, generadores) {
+    return generadores.generadorNormalValidacion.generar(rnd1, rnd2);
+}
+
+function determinarDemoraBloqueo(datos,demoraBase) {
+    return demoraBase * datos.porcAumentoDemora / 100;
+}
+function determinarDemoraParadaExtra(rnd,generadores) {
+    return generadores.generadorExponencialParadaExtra.generar(rnd)[0];
+}
+
+function procesarFila(filaAnterior, datos, generadores) {
+  const filaActual = [...filaAnterior]; // copiar fila anterior
+
+  filaActual[0] = filaAnterior[0] ? filaAnterior[0] + 1 : 1; // Vehículo
+  const rndSector = generarRND();
+  filaActual[1] = rndSector; // RND1
+  filaActual[2] = determinarSectorDestino(rndSector, datos); // Sector destino
+  const sectorDestino = determinarSectorDestino(rndSector, datos);
+  filaActual[2] = sectorDestino; // Sector
+  const rndCuadras = generarRND(); // RND 2
+  const cuadrasAlDestino = determinarCuadrasAlDestino(
+    sectorDestino,
+    rndCuadras,
+    generadores,
+  );
+  filaActual[3] = rndCuadras; // RND 2
+  filaActual[4] = cuadrasAlDestino; // Cuadras
+  const rndRecorrido = generarRND(); // RND 3
+  filaActual[5] = rndRecorrido; // RND 3
+  const tiempoXCuadra =
+    generadores.generadorUniformeRecorrido.generar(rndRecorrido)[0];
+  filaActual[6] = tiempoXCuadra; // Tiempo por cuadra
+  const demoraBase = tiempoXCuadra * cuadrasAlDestino;
+  filaActual[7] = demoraBase; // Demora base
+  const rndValidacion = generarRND(); // RND 4
+  filaActual[8] = rndValidacion; //RND 4 
+  const paradaValidacion = derminarParadaValidacion(rndValidacion, datos);
+  filaActual[9] = paradaValidacion ? 'Sí' : 'No'; 
+    
+     // Actualizar las demoras de validación disponibles
+        if (filaAnterior[9] === 'Sí') {
+        // Si hubo validación en la fila anterior y se generaron demoras, se utilzó la demora de validación 1 (filaAnterior[12])
+        if (filaAnterior[12] !== 0 && filaAnterior[13] !== 0) {
+            filaActual[12] = 0;
+            filaActual[13] = filaAnterior[13]; // Demora validación 2 se mantiene
+        } else {
+            // Si no se generaron demoras en la fila anterior, se utilizo un demora de validación 2 y ambas quedan en cero
+            filaActual[12] = 0;
+            filaActual[13] = 0;
+        }
+    }
+    
+    if (filaActual[9] === 'Sí') {
+      // Si se tiene una demora de validación y no hay una demora remanente (Demora de validación 2) en la fila anterior, se generan las demoras de validación 1 y 2
+      if (filaActual[13] === 0) {
+        // generar las demoras
+        const rnd1DemoraValidacion = generarRND(); // RND 5
+        const rnd2DemoraValidacion = generarRND(); // RND 6
+        filaActual[10] = rnd1DemoraValidacion; // RND 5
+        filaActual[11] = rnd2DemoraValidacion; // RND 6
+        const arrayDemorasValidacion = determinarDemoraValidacion(
+          rnd1DemoraValidacion,
+          rnd2DemoraValidacion,
+          generadores,
+        );
+        filaActual[12] = arrayDemorasValidacion[0]; // Demora validación 1
+        filaActual[13] = arrayDemorasValidacion[1]; // Demora validación 2
+      }
+    }  else {
+        filaActual[10] = '-'; // RND 5 vacío si no hay parada en validación
+        filaActual[11] = '-'; // RND 6 mantiene el valor anterior si no hay parada en validación
+    }
+  
+    let bloqueoRuta = false;
+
+    if (['Cercano', 'Lejano'].includes(sectorDestino)) {
+        const rndBloqueo = generarRND(); 
+        filaActual[14] = rndBloqueo; // RND 7 
+        bloqueoRuta = determinarBloqueoRuta(rndBloqueo, datos);
+        filaActual[15] = bloqueoRuta ? 'Sí' : 'No'; // ¿Ruta bloqueada?
+        if (bloqueoRuta) {
+            const incrementoDemora = determinarDemoraBloqueo(datos, demoraBase); 
+            filaActual[16] = incrementoDemora; // Incremento 
+        } else {
+            filaActual[16] = 0; // Sin incremento de demora
+        }
+              
+    } else{
+        filaActual[14] = '-'; // RND 7 vacío para sector intermedio
+        filaActual[15] = '-'; // ¿Ruta Bloqueada? vacía para sector intermedio
+        filaActual[16] = 0; // Incremento nulo para sector intermedio
+    }
+
+    const rndParadaExtra = generarRND(); // RND 8
+    filaActual[18] = rndParadaExtra; // RND 8
+    const paradaExtra = determinarParadaExtra(rndParadaExtra, datos);
+    filaActual[19] = paradaExtra ? 'Sí' : 'No'; // ¿Hace Parada Extra?
+    if (paradaExtra) {
+        const rndDemoraParadaExtra = generarRND(); // RND 9 
+        filaActual[20] = rndDemoraParadaExtra; // RND 9
+        const demoraParadaExtra = determinarDemoraParadaExtra(rndDemoraParadaExtra, generadores);
+        filaActual[2] = demoraParadaExtra; // Demora por parada extra
+    } else {
+        filaActual[19] = '-'; // RND 9 vacío si no hace parada extra
+        filaActual[20] = 0; // Sin demora por parada extra
+    }
+
+    let tiempoTotal = 0;
+
+    if(filaActual[12]!= 0){
+        tiempoTotal =
+          demoraBase + filaActual[12] + filaActual[16] + filaActual[20];
+    }
+    else{
+        tiempoTotal = demoraBase + filaActual[13] + filaActual[16] + filaActual[20];
+    }
+    filaActual[21] = tiempoTotal; // Tiempo total 
+
+    //Acumuladores
+    const tiempoTotalAcumulado = filaAnterior[22] ? filaAnterior[22] + filaActual[21] : filaActual[21];
+    filaActual[22] = tiempoTotalAcumulado; // Tiempo++
+
+    const contadorDetencionesDobles = filaAnterior[23] + (paradaValidacion && paradaExtra ? 1 : 0);
+    filaActual[23] = contadorDetencionesDobles; // Detenciones dobles++
+
+    const maxTiempo = filaAnterior[24] ? Math.max(filaAnterior[24], filaActual[21]) : filaActual[21];
+    filaActual[24] = maxTiempo; // Max Tiempo
+
+    const minTiempo = filaAnterior[25] ? Math.min(filaAnterior[25], filaActual[21]) : filaActual[21];
+    filaActual[25] = minTiempo; // Min Tiempo
+
+    const contadorVehiculosDemorados = filaAnterior[26] ? (paradaValidacion || paradaExtra || bloqueoRuta ? 1 : 0) + filaAnterior[26] : (paradaValidacion || paradaExtra || bloqueoRuta ? 1 : 0);
+    filaActual[26] = contadorVehiculosDemorados; // Vehículos demorados++
+
+    const contadorVehiculosCercano = filaAnterior[27] ? filaAnterior[27] + (sectorDestino === 'Cercano' ? 1 : 0) : (sectorDestino === 'Cercano' ? 1 : 0);
+    filaActual[27] = contadorVehiculosCercano; // Contador Vehículos Cercano
+
+    const contadorVehiculosIntermedio = filaAnterior[28] ? filaAnterior[28] + (sectorDestino === 'Intermedio' ? 1 : 0) : (sectorDestino === 'Intermedio' ? 1 : 0);
+    filaActual[28] = contadorVehiculosIntermedio; // Contador Vehículos Intermedio
+
+    const contadorVehiculosLejanos = filaAnterior[29] ? filaAnterior[29] + (sectorDestino === 'Lejano' ? 1 : 0) : (sectorDestino === 'Lejano' ? 1 : 0);
+    filaActual[29] = contadorVehiculosLejanos; // Contador Vehículos Lejano
+
+    const acumuladorVehiculosCercano = filaAnterior[30] ? filaAnterior[30] + (sectorDestino === 'Cercano' ? filaActual[21] : 0) : (sectorDestino === 'Cercano' ?  filaActual[21] : 0);
+    filaActual[30] = acumuladorVehiculosCercano; // Tiempo Acumulado Vehículos Cercano
+
+    const acumuladorVehiculosIntermedio = filaAnterior[31] ? filaAnterior[31] + (sectorDestino === 'Intermedio' ? filaActual[21] : 0) : (sectorDestino === 'Intermedio' ? filaActual[21] : 0);
+    filaActual[31] = acumuladorVehiculosIntermedio; // Tiempo Acumulado Vehículos Intermedio
+
+    const acumuladorVehiculosLejanos = filaAnterior[32] ? filaAnterior[32] + (sectorDestino === 'Lejano' ? filaActual[21]: 0) : (sectorDestino === 'Lejano' ? filaActual[21] : 0);
+    filaActual[32] = acumuladorVehiculosLejanos; // Tiempo Acumulado Vehículos Lejano
+
+    let demoraMaxValidacion = 0;
+
+    if(filaActual[12]!= 0){
+        demoraMaxValidacion = filaAnterior[33] ? Math.max(filaAnterior[33], filaActual[12]) : filaActual[12];
+    } else{
+        demoraMaxValidacion = filaAnterior[33] ? Math.max(filaAnterior[33], filaActual[13]) : filaActual[13];
+    }
+    
+    filaActual[33] = demoraMaxValidacion; // Demora Máxima en Validación
+
+    const demoraMaxBloqueo = filaAnterior[34] ? Math.max(filaAnterior[34], filaActual[16]) : filaActual[16];
+    filaActual[34] = demoraMaxBloqueo; // Demora Máxima por Bloqueo
+
+    const demoraMaxParadaExtra = filaAnterior[35] ? Math.max(filaAnterior[35], filaActual[20]) : filaActual[20];
+    filaActual[35] = demoraMaxParadaExtra; // Demora Máxima por Parada Extra    
+
+}
+
+
+function generarReporte(ultimaFila) {
+    const N = [ultimaFila][0] + 1; // indice + 1
+
+    const resultados = {
+        punto1: ultimaFila[22] / N, // Tiempo promedio desde el ingreso del vehículo hasta su estacionamiento
+        punto2: ultimaFila[23] / N * 100, 
+        punto3: N - ultimaFila[23],
+        punto4: ultimaFila[24],
+        punto5: ultimaFila[25],
+        punto6: ultimaFila[26],
+        punto7A: ultimaFila[40] / ultimaFila[27],
+        punto7B: ultimaFila[41] / ultimaFila[28],
+        punto7C: ultimaFila[42] / ultimaFila[29],
+        punto8A: ultimaFila[43],
+        punto8B: ultimaFila[44],
+        punto8C: ultimaFila[45],
+    };
+
+    return resultados;
+}
